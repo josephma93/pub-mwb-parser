98c5a1d46a145da432c2f29b338fb384afd91ee6
Joseph Montero <3136168+josephma93@users.noreply.github.com>
2024-09-10 13:56:14 -0600

    feat(logger.mjs): add stderr stream to logger configuration for improved error logging

--- src/core/logger.mjs
+++ src/core/logger.mjs
@@ -2,9 +2,14 @@ import pino from 'pino';
 
 const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
 
+const stderrStream = pino.destination(2);
+
 // Configuración e inicialización del logger
-const logger = pino({
-    level: LOG_LEVEL,
-});
+const logger = pino(
+    {
+        level: LOG_LEVEL,
+    },
+    stderrStream
+);
 
 export default logger;
